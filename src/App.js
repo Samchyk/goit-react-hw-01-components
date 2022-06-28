385d367de07023fbf6ba53743dd9c589af7057ad
Oleksii Samchyk <Aleksei.samchik@gmail.com>
2022-06-28 14:07:24 +0300

    Profile

---
+++ src/App.js
@@ -0,0 +1,32 @@
+
+import Profile from './components/Profile/Profile';
+import user from './data/user.json';
+
+const { username, tag, location, avatar, stats } = user;
+const App = () => {
+   return (
+      <div
+         style={{
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'center',
+            alignItems: 'center',
+            fontSize: 40,
+            color: '#010101',
+            paddingTop: '30px',
+         }}
+      >
+         <Profile
+            username={username}
+            tag={tag}
+            location={location}
+            avatar={avatar}
+            stats={stats}
+         />
+      </div>
+   );
+};
+
+export default App;
+
+
