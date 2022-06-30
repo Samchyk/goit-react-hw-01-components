a7b6faa182dbaa95afaa706793038600635c566e
Oleksii Samchyk <Aleksei.samchik@gmail.com>
2022-06-30 13:24:50 +0300

    1

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@
 import Profile from './components/Profile/Profile';
 import user from './data/user.json';
 
+
+
 const { username, tag, location, avatar, stats } = user;
 const App = () => {
    return (
