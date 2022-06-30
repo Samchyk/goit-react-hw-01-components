c1244571d132268f9fb597a4b0808e1571b2c37a
Oleksii Samchyk <Aleksei.samchik@gmail.com>
2022-06-30 13:58:56 +0300

    task 2

--- src/App.js
+++ src/App.js
@@ -1,34 +1,33 @@
-
 import Profile from './components/Profile/Profile';
 import user from './data/user.json';
-
-
+import Statistics from 'components/Statistics/Statistics';
+import data from './data/data.json';
 
 const { username, tag, location, avatar, stats } = user;
 const App = () => {
-   return (
-      <div
-         style={{
-            display: 'flex',
-            flexDirection: 'column',
-            justifyContent: 'center',
-            alignItems: 'center',
-            fontSize: 40,
-            color: '#010101',
-            paddingTop: '30px',
-         }}
-      >
-         <Profile
-            username={username}
-            tag={tag}
-            location={location}
-            avatar={avatar}
-            stats={stats}
-         />
-      </div>
-   );
+  return (
+    <div
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: 40,
+        color: '#010101',
+        paddingTop: '30px',
+      }}
+    >
+      <Profile
+        username={username}
+        tag={tag}
+        location={location}
+        avatar={avatar}
+        stats={stats}
+      />
+      <Statistics title="Upload stats" stats={data} />
+      <Statistics stats={data} />
+    </div>
+  );
 };
 
 export default App;
-
-
